119e9ba04bc5282e907074ddba8160a609085cc0
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-06 20:59:24 +0200

    register and login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {Button, ButtonDirective} from 'primeng/button';
+import {InputText} from 'primeng/inputtext';
+import {Divider} from 'primeng/divider';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, InputText, Divider, ButtonDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {Divider} from 'primeng/divider';
+import {ButtonDirective} from 'primeng/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    Divider,
+    ButtonDirective
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
