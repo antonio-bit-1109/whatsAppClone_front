00366188d8c66ed48c0cc48c79db29109b3f606b
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-24 12:35:51 +0200

    Implement chat editor functionality with initial setup and editor instance creation

--- src/app/components/chat-editor/chat-editor.component.ts
+++ src/app/components/chat-editor/chat-editor.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, OnDestroy} from '@angular/core';
+// @ts-ignore
+import Editor from '@toast-ui/editor';
+
 
 @Component({
   selector: 'app-chat-editor',
@@ -6,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './chat-editor.component.html',
   styleUrl: './chat-editor.component.scss'
 })
-export class ChatEditorComponent {
+export class ChatEditorComponent implements OnInit {
+  public editorInstance: any;
+
+  ngOnInit() {
+
+    const editor = new Editor({
+      el: document.querySelector('#editor'),
+      height: '200px',
+      initialEditType: 'wysiwyg',
+      initialValue: "Inserisci messaggio.",
+      previewStyle: 'tab',
+    });
+    this.editorInstance = editor;
+    editor.getMarkdown();
+  }
+
+  // submitForm() {
+  //   console.log(this.editorInstance.getMarkdown());
+  //   console.log((this.editorInstance as Editor).getHTML());
+  // }
 
 }
