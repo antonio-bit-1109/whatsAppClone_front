fd9a0d8a6a9ab9b9571bd86ed1e3bf771ffcdfe2
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-27 18:14:03 +0200

    Add WebSocket connection service and enhance chat editor functionality

---
+++ src/app/services/handle-web-socket-connection.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+// @ts-ignore
+import SockJS from 'sockjs-client';
+import {Stomp} from '@stomp/stompjs';
+import {AuthService} from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HandleWebSocketConnectionService {
+
+  private stompClient: any;
+
+  constructor(private authService: AuthService) {
+  }
+
+
+  // connect to webSocket all avvio dellapplicazione ??
+  public connectToServerSocket(serverPort: string) {
+    console.log("try to connecting web socket ...")
+
+    const token = this.authService.getToken();
+
+    // passando il token al server faccio un ulteriore controllo che l'utente che si sta collegando sia un utente autenticato gia in precedenza
+    const socket = new SockJS(`http://localhost:${serverPort}/ws?token=${token}`);
+    this.stompClient = Stomp.over(socket);
+
+    this.stompClient.connect({}, () => {
+      console.log("WebSocket connected!");
+
+    }, (error: any) => {
+      console.error("WebSocket connection error:", error);
+    });
+
+  }
+
+
+  public subscribeToPrivateChat(chatIdentity: string) {
+    this.stompClient.subscribe(`/chat-private/${chatIdentity}`, (message: any) => {
+      console.log("collegato alla chat: " + chatIdentity);
+    })
+  }
+}
