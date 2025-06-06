be446df7e5940e9ca5eed6c3b400ed198c8282c7
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-06-06 18:16:52 +0200

    Refactor layout structure in chat and home components for improved responsiveness and styling

---
+++ src/app/components/handle-emails/handle-emails.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {SendMeMessagesService} from '../../services/send-me-messages.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {IMessageSent} from '../../interfaces/SendMeMessage';
+import {ToastMessageService} from '../../services/toast-message.service';
+import {Panel} from 'primeng/panel';
+import {NgForOf, NgIf} from '@angular/common';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-handle-emails',
+  imports: [
+    Panel,
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './handle-emails.component.html',
+  styleUrl: './handle-emails.component.scss'
+})
+export class HandleEmailsComponent implements OnInit {
+
+  protected allMEssagesSent: IMessageSent[] = []
+  protected selected: IMessageSent | null = null;
+
+  constructor(private sendMeMessageService: SendMeMessagesService,
+              private toastService: ToastMessageService) {
+  }
+
+  ngOnInit() {
+    this.sendMeMessageService.getAllEmailsSentToMe().subscribe({
+      next: (resp) => {
+        this.allMEssagesSent = resp;
+        this.allMEssagesSent.length === 0 && this.toastService.show("info", "email", "nessuna email da visualizzare.")
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err.error)
+        this.toastService.show("error", "errore reperimento messaggi", "impossibile reperire le email.")
+      }
+    })
+  }
+}
